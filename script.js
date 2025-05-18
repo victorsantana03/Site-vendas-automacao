80ff83745dc11e0878d500e15c73f661d84e1d93
victorsantana03 <viclucigei.123@gmail.com>
2025-05-18 01:52:12 -0300

    estilização

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const header = document.querySelector("#header");
+const modules = document.querySelector(".modules");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
-    header.classList.add("shadow-lg");
+    header.classList.add("shadow-md");
   } else {
-    header.classList.remove("shadow-lg");
+    header.classList.remove("shadow-md");
+  }
+});
+
+modules.addEventListener("click", function (e) {
+  const event = e.target;
+  const click = event.closest(".module");
+
+  if (click) {
+    const list = click.querySelector(".list");
+    const icon = click.querySelector(".fa-solid");
+    icon.classList.toggle("fa-chevron-right");
+    icon.classList.toggle("fa-chevron-down");
+    list.classList.toggle("hidden");
+    click.classList.toggle("rounded-full");
+    click.classList.toggle("rounded-2xl");
   }
 });
