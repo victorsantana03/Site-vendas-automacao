50f1bf9660c44dd07148797ff76d61ac864f50d6
victorsantana03 <viclucigei.123@gmail.com>
2025-05-21 23:17:20 -0300

    fim responsividade

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const header = document.querySelector("#header");
 const modules = document.querySelector(".modules");
+const btnMenu = document.querySelector("#btn-menu");
+const btnCloseMenu = document.querySelector("#btn-close-menu");
+const menu = document.querySelector("#menu");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
@@ -21,5 +24,38 @@ modules.addEventListener("click", function (e) {
     list.classList.toggle("hidden");
     click.classList.toggle("rounded-full");
     click.classList.toggle("rounded-2xl");
+  }
+});
+
+btnMenu.addEventListener("click", function () {
+  menu.classList.remove("hidden");
+  menu.classList.add("flex");
+  setTimeout(() => {
+    menu.classList.toggle("opacity-0");
+    document
+      .querySelector("#menu-content")
+      .classList.toggle("translate-x-full");
+  }, 10);
+});
+
+btnCloseMenu.addEventListener("click", function () {
+  menu.classList.toggle("opacity-0");
+  document.getElementById("menu-content").classList.toggle("translate-x-full");
+
+  setTimeout(() => {
+    menu.classList.toggle("hidden");
+  }, 300);
+});
+
+menu.addEventListener("click", function (event) {
+  clique = event.target;
+  if (clique.classList.contains("menu")) {
+    document
+      .getElementById("menu-content")
+      .classList.toggle("translate-x-full");
+
+    setTimeout(() => {
+      menu.classList.toggle("hidden");
+    }, 300);
   }
 });
